d68cfeb7840fd52b27881e847634baa48af68a08
Kim Kolis <kimberlyholland13@gmail.com>
2021-08-09 23:29:30 -0400

    create Introduction

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,15 @@
-
 import './App.css';
 import Signup from './Component/Signup';
 import { Router } from '@reach/router';
+import Introduction from './Component/Introduction';
 
 function App() {
   return (
     <div className="App">
       <Router>
-     <Signup path='/' />
-     </Router>
+        <Signup path='/' />
+        <Introduction path='/Introduction' />
+      </Router>
     </div>
   );
 }
---
+++ client/src/Component/Introduction.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import axios from "axios";
+import { navigate } from '@reach/router';
+
+const Introduction = () => {
+    return (
+        <div>
+            <h1>Introduction</h1>
+            <p>This site is for anyone who is struggling with mental illness, or just having a bad day, come over
+here and blog, what's on your mind. This site is made to provide information to those to who need
+help and can't find the right resources. This site is a place to leave your information or phone
+number; if possible we will try to give you some resouces with information, and also groups that are
+available to join on media websites. Lastly , you can blog on here about your day to relieve the
+pressure and support each other, and also blog about good things as well, so just like a rainbow,
+there's always rains. No bullying, sexism, racism, etc will be tolerated on this site..Thank you. Please
+rank sites that have work the best for you and leave comments</p>       
+
+          <h5>Thanks....</h5>
+        </div>
+    );
+};
+
+export default Introduction;
